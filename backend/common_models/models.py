0f367741b43e6e4250145071af6e3d9d918b5f16
RoguedBear <39442192+RoguedBear@users.noreply.github.com>
2023-02-15 18:35:33 +0530

    initial migrations, added Teacher & Subject model
    making computed fields and defining relationships still pending

--- backend/common_models/models.py
+++ backend/common_models/models.py
@@ -1,3 +1,52 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
+
+# TODO: create computed fields
+# TODO: add many to many relationships
+
+
+class AllottmentStatus(models.TextChoices):
+    NONE = "NONE"
+    PARTIAL = "PART"
+    FULL = "FULL"
+
+
+class Subject(models.Model):
+
+    class CourseType(models.TextChoices):
+        CORE = "CORE"
+        ELECTIVE = "ELEC"
+
+    name = models.CharField(max_length=128)
+    course_code = models.SlugField(max_length=10)
+    credits = models.SmallIntegerField()  # maybe add max length?
+    course_type = models.CharField(max_length=4, choices=CourseType.choices)
+    programme = models.CharField(max_length=20)
+
+    original_lecture_hours = models.SmallIntegerField()
+    original_tutorial_hours = models.SmallIntegerField()
+    original_practical_hours = models.SmallIntegerField()
+
+    number_of_lecture_batches = models.SmallIntegerField()
+    number_of_practical_or_tutorial_batches = models.SmallIntegerField()
+
+    allotment_status = models.CharField(
+        max_length=4, choices=AllottmentStatus.choices, default=AllottmentStatus.NONE)
+
+
+class Teacher(models.Model):
+
+    class PrefferredMode(models.TextChoices):
+        LECTURE = "LEC", _('Lecture')
+        TUTORIAL = "TUT", _('Tutorial')
+        PRACTICAL = "LAB", _("Practical/Lab")
+        ANY = "ANY", _("Any")
+
+    name = models.CharField(max_length=128)
+
+    prefferred_mode = models.CharField(
+        max_length=3, choices=PrefferredMode.choices, default=PrefferredMode.ANY)
+    assigned_status = models.CharField(
+        max_length=4, choices=AllottmentStatus.choices, default=AllottmentStatus.NONE)
--- backend/ez_allocate_backend/settings.py
+++ backend/ez_allocate_backend/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'common_models',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
