1b41271c6b196c8b912f21756cd55288fb441e87
RoguedBear <39442192+RoguedBear@users.noreply.github.com>
2023-02-16 00:30:11 +0530

    added choices & allotment relation & refactoring - adding many to many relationships B/w teacher & subject     Refactoring: - fixing typos - formatting stuff

--- backend/common_models/models.py
+++ backend/common_models/models.py
@@ -1,21 +1,20 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
+
 # Create your models here.
 
 # TODO: create computed fields
-# TODO: add many to many relationships
 # TODO: define max/min values for integer field
 
 
-class AllottmentStatus(models.TextChoices):
+class AllotmentStatus(models.TextChoices):
     NONE = "NONE"
     PARTIAL = "PART", _('Partial')
     FULL = "FULL"
 
 
 class Subject(models.Model):
-
     class CourseType(models.TextChoices):
         CORE = "CORE"
         ELECTIVE = "ELEC"
@@ -34,12 +33,15 @@ class Subject(models.Model):
     number_of_practical_or_tutorial_batches = models.SmallIntegerField()
 
     allotment_status = models.CharField(
-        max_length=4, choices=AllottmentStatus.choices, default=AllottmentStatus.NONE)
+        max_length=4, choices=AllotmentStatus.choices, default=AllotmentStatus.NONE)
+    allotted_teachers = models.ManyToManyField("Teacher", through="Allotment")
+
+    def __str__(self):
+        return f"{self.course_code}: {self.name}"
 
 
 class Teacher(models.Model):
-
-    class PrefferredMode(models.TextChoices):
+    class PreferredMode(models.TextChoices):
         LECTURE = "LEC", _('Lecture')
         TUTORIAL = "TUT", _('Tutorial')
         PRACTICAL = "LAB", _("Practical/Lab")
@@ -47,7 +49,34 @@ class Teacher(models.Model):
 
     name = models.CharField(max_length=128)
 
-    prefferred_mode = models.CharField(
-        max_length=3, choices=PrefferredMode.choices, default=PrefferredMode.ANY)
+    preferred_mode = models.CharField(
+        max_length=3, choices=PreferredMode.choices, default=PreferredMode.ANY)
     assigned_status = models.CharField(
-        max_length=4, choices=AllottmentStatus.choices, default=AllottmentStatus.NONE)
+        max_length=4, choices=AllotmentStatus.choices, default=AllotmentStatus.NONE)
+
+    subject_choices = models.ManyToManyField(Subject, through="Choices")
+
+    def __str__(self):
+        return self.name
+
+
+class Choices(models.Model):
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+    choice_number = models.SmallIntegerField()
+
+    def __str__(self):
+        return f"teacher={self.teacher.name}, subject={self.subject}, choice={self.choice_number}"
+
+
+class Allotment(models.Model):
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+
+    allotted_lectures_hours = models.SmallIntegerField()
+    allotted_tutorial_hours = models.SmallIntegerField()
+    allotted_practical_hours = models.SmallIntegerField()
+
+    def __str__(self):
+        return f"teacher={self.teacher.name}, subject={self.subject.name}, lecture={self.allotted_lectures_hours}, " \
+               f"tutorial={self.allotted_tutorial_hours}, practical={self.allotted_practical_hours}"
