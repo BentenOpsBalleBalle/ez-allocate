6a364358d5181e88326876b19308e60c18fb56dc
RoguedBear <39442192+RoguedBear@users.noreply.github.com>
2023-02-16 02:47:29 +0530

    added computed fields for Subject & Teacher
    - added a default allotment hour of 0
    - renamed allotted_lectures_hours to singular, alloted_lecture_hours

--- backend/common_models/models.py
+++ backend/common_models/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from django.utils.functional import cached_property
 from django.utils.translation import gettext_lazy as _
 
 
 # Create your models here.
 
-# TODO: create computed fields
+# TODO: test these out aka write unit tests maybe
 # TODO: define max/min values for integer field
 
 
@@ -14,6 +15,11 @@ class AllotmentStatus(models.TextChoices):
     FULL = "FULL"
 
 
+# NOTE: can add custom QuerySet as managers to filter computed fields
+# source: https://stackoverflow.com/a/36996962
+# remember:
+#   - properties -> row-level functionality
+#   - manager methods -> table-level functionality
 class Subject(models.Model):
     class CourseType(models.TextChoices):
         CORE = "CORE"
@@ -39,6 +45,37 @@ class Subject(models.Model):
     def __str__(self):
         return f"{self.course_code}: {self.name}"
 
+    @property
+    def total_lecture_hours(self):
+        return self.original_lecture_hours * self.number_of_lecture_batches
+
+    @property
+    def total_tutorial_hours(self):
+        return self.original_tutorial_hours * self.number_of_practical_or_tutorial_batches
+
+    @property
+    def total_practical_hours(self):
+        return self.original_practical_hours * self.number_of_practical_or_tutorial_batches
+
+    @cached_property
+    def __allotted_hours_computed(self):
+        return self.allotment_set.aggregate(lecture=models.Sum('allotted_lecture_hours'),
+                                            tutorial=models.Sum(
+                                                'allotted_tutorial_hours'),
+                                            practical=models.Sum('allotted_practical_hours'))
+
+    @property
+    def allotted_lecture_hours(self):
+        return self.__allotted_hours_computed["lecture"]
+
+    @property
+    def allotted_tutorial_hours(self):
+        return self.__allotted_hours_computed["tutorial"]
+
+    @property
+    def allotted_practical_hours(self):
+        return self.__allotted_hours_computed["practical"]
+
 
 class Teacher(models.Model):
     class PreferredMode(models.TextChoices):
@@ -59,6 +96,12 @@ class Teacher(models.Model):
     def __str__(self):
         return self.name
 
+    @property
+    def current_load(self):
+        return self.allotment_set.aggregate(load=models.Sum('allotted_lecture_hours') +
+                                            models.Sum('allotted_tutorial_hours') +
+                                            models.Sum('allotted_practical_hours'))
+
 
 class Choices(models.Model):
     teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
@@ -73,10 +116,10 @@ class Allotment(models.Model):
     teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
     subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
 
-    allotted_lectures_hours = models.SmallIntegerField()
-    allotted_tutorial_hours = models.SmallIntegerField()
-    allotted_practical_hours = models.SmallIntegerField()
+    allotted_lecture_hours = models.SmallIntegerField(default=0)
+    allotted_tutorial_hours = models.SmallIntegerField(default=0)
+    allotted_practical_hours = models.SmallIntegerField(default=0)
 
     def __str__(self):
-        return f"teacher={self.teacher.name}, subject={self.subject.name}, lecture={self.allotted_lectures_hours}, " \
+        return f"teacher={self.teacher.name}, subject={self.subject.name}, lecture={self.allotted_lecture_hours}, " \
                f"tutorial={self.allotted_tutorial_hours}, practical={self.allotted_practical_hours}"
