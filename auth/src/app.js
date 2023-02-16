ab17e37ce6c3ded1e2b006a6abfaaeaf0a1343ff
chiggy69 <chiragaggarwal69@gmail.com>
2023-02-16 19:04:07 +0530

    initialize auth service with fake data

---
+++ auth/src/app.js
@@ -0,0 +1,15 @@
+import express from "express";
+import cors from "cors";
+import { subjects } from "../temporary/subjects.js";
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cors());
+
+app.get("/test", (req, res) => {
+	res.send(subjects);
+});
+
+export default app;
---
+++ auth/src/server.js
@@ -0,0 +1,9 @@
+import app from "./app.js";
+
+const startServer = async () => {
+	app.listen(3000, () => {
+		console.log("Listening on port 3000");
+	});
+};
+
+startServer();
