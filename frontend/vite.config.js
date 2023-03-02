e7bf491f4f40cbcd958e32818370d0246b99c3f1
chiggy69 <chiragaggarwal69@gmail.com>
2023-03-02 20:19:09 +0530

    rerender on saving in docker

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
+import dotenv from "dotenv";
+dotenv.config();
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    define: {
+        "process.env": process.env, // Pass environment variables to the app
+    },
+    server: {
+        watch: {
+            usePolling: true,
+        },
+    },
 });
