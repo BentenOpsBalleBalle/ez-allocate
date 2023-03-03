c5c59751d0459cf51c3201365a5eeb093d4e3ff6
RoguedBear <39442192+RoguedBear@users.noreply.github.com>
2023-03-03 17:01:22 +0530

    whitelisted vite 5173 port

--- backend/ez_allocate_backend/settings.py
+++ backend/ez_allocate_backend/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "http://localhost:5173",
+    "http://127.0.0.1:5173"
 ]
 
 # Application definition
