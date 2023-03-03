018abb678d1961ccbac0457aa6304c087bee055c
RoguedBear <39442192+RoguedBear@users.noreply.github.com>
2023-03-03 16:59:11 +0530

    add whitelisted cors to backned

--- backend/ez_allocate_backend/settings.py
+++ backend/ez_allocate_backend/settings.py
@@ -27,6 +27,11 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+# CORS SETTINGS
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+]
 
 # Application definition
 
@@ -34,6 +39,7 @@ INSTALLED_APPS = [
     'common_models',
     'api',
     'rest_framework',
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -43,6 +49,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
