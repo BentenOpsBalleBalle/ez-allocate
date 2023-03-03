917b14da231beff7c5c619a004af2934af61f277
RoguedBear <39442192+RoguedBear@users.noreply.github.com>
2023-03-04 00:36:10 +0530

    remove unused imports

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,6 +1,6 @@
 from common_models import serializers
 from common_models.models import Allotment, Choices, Subject, Teacher
-from rest_framework import generics, viewsets
+from rest_framework import viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
 
--- backend/ez_allocate_backend/urls.py
+++ backend/ez_allocate_backend/urls.py
@@ -15,9 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from django.views.generic import TemplateView
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
-from rest_framework.schemas import get_schema_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
