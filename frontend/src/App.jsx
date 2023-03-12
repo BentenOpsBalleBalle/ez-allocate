b5a7eca9d7ce0f9b0e77d6b655cfdc13169b5306
chiggy69 <chiragaggarwal69@gmail.com>
2023-03-12 16:38:41 +0530

    new routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,15 @@
-import SubjectList from "./components/SubjectList";
+import SubjectsPage from "./Pages/SubjectsPage";
+import Subject from "./components/Subject";
 import LoginPage from "./Pages/LoginPage";
 import TeachersPage from "./Pages/TeachersPage";
 import Teacher from "./components/Teacher";
 import { Routes, Route } from "react-router-dom";
 const App = () => {
-    console.log("hello");
-    const en = process.env.REACT_APP_AUTH_URL;
-    console.log(en, "enviroment variable");
     return (
         <div className="w-screen h-screen">
             <Routes>
-                <Route path="/subjects" element={<SubjectList />} />
+                <Route path="/subjects" element={<SubjectsPage />} />
+                <Route path="/subjects/:id" element={<Subject />} />
 
                 <Route path="/teachers" element={<TeachersPage />} />
                 <Route path="/teachers/:id" element={<Teacher />} />
