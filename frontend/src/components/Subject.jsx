0c97000b64d0f42e0795f164ede5e515f0eb6db9
chiggy69 <chiragaggarwal69@gmail.com>
2023-03-19 18:04:06 +0530

    Subject page

--- frontend/src/components/Subject.jsx
+++ frontend/src/components/Subject.jsx
@@ -1,12 +1,21 @@
+//how to remove manually added teacher
+
 import Client from "../helpers/Client";
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import AssignTeacherCard from "./Subject Components/AssignTeacherCard";
+import { useParams } from "react-router-dom";
+import { useQueryClient } from "@tanstack/react-query";
+import { Spinner, useToasts } from "@geist-ui/core";
+import { CustomSearch } from "./common/CustomSearch";
 
 const client = new Client();
 
 const Subject = () => {
-    const id = 1;
-    const subjectQuery = useQuery(["subjects", id], () =>
+    const queryClient = useQueryClient();
+    const { id } = useParams();
+    const { setToast } = useToasts();
+
+    const subjectQuery = useQuery(["subjects", +id], () =>
         client.createUrl({
             url: `api/subjects/${id}`,
             method: "GET",
@@ -14,40 +23,93 @@ const Subject = () => {
         })
     );
 
-    const choiceQuery = useQuery(["subjects", id, "choices"], () =>
-        client.createUrl({
+    const choiceQuery = useQuery(["subjects", +id, "choices"], () => {
+        console.log("okokokok");
+        return client.createUrl({
             url: `api/subjects/${id}/choices`,
             method: "GET",
             service: "allocate",
-        })
+        });
+    });
+
+    const addTeacherMutation = useMutation(
+        (teacherId) => {
+            return client.createUrl({
+                url: `api/subjects/${id}/choices/modify/${teacherId}/`,
+                method: "POST",
+                service: "allocate",
+            });
+        },
+        {
+            onSuccess: (data) => {
+                queryClient.refetchQueries(["subjects", +id, "choices"], {
+                    exact: true,
+                });
+                setToast({
+                    text: "Teacher added successfully",
+                    type: "success",
+                });
+            },
+            onError: (error) => {
+                setToast({
+                    text: error.response.data.non_field_errors,
+                    type: "error",
+                });
+            },
+        }
     );
 
-    const { data, isLoading, error } = subjectQuery;
+    // const addTeacher = async () => {
+    //     const resp = await client.createUrl({
+    //         url: `api/subjects/${id}/choices/modify/${1}/`,
+    //         method: "POST",
+    //         service: "allocate",
+    //     });
+    //     console.log(resp);
+    //     queryClient.invalidateQueries(["subjects", id, "choices"], {
+    //         exact: true,
+    //     });
+    // };
+
+    // const choiceLabels = {};
+    const { isLoading, error } = subjectQuery;
     if (isLoading) return <div>Loading..</div>;
     if (error) return <div>Error: {error.message}</div>;
-
     return (
         <div>
-            <div>Assign teachers</div>
+            <div>Assign teachers to {subjectQuery.data.data.name}</div>
+            {/* {console.log(subjectQuery.data.data)} */}
+            <div>{subjectQuery.data.data.total_lecture_hours} lecture</div>
+            <div>{subjectQuery.data.data.total_tutorial_hours} tutorial</div>
+            <div>{subjectQuery.data.data.total_practical_hours} practical</div>
+            <CustomSearch
+                searchFor="teachers"
+                onSelect={(value) => {
+                    addTeacherMutation.mutate(value);
+                }}
+                width="300px"
+            />
             {choiceQuery.isLoading ? (
-                <p>.?..</p>
+                <Spinner />
             ) : (
                 <div>
-                    {/* {console.log(choiceQuery.data.data)} */}
-                    <div className="flex flex-col gap-y-4">
+                    <div className="flex gap-8 flex-wrap">
                         {choiceQuery.data.data.map((choice) => {
                             return (
                                 <AssignTeacherCard
                                     key={choice.teacher.id}
                                     choice_number={choice.choice_number}
                                     teacher={choice.teacher}
-                                    subjectData={data.data}
+                                    subjectData={subjectQuery.data.data}
                                 />
                             );
                         })}
                     </div>
                 </div>
             )}
+            {/* <button onClick={() => addTeacherMutation.mutate()}>
+                Manual Teacher
+            </button> */}
         </div>
     );
 };
