d4d017615326dc2d8e0c38a1a6ebfec9322f82a6
chiggy69 <chiragaggarwal69@gmail.com>
2023-03-20 09:57:15 +0530

    quality improvements

--- frontend/src/components/Teacher Components/Tile.jsx
+++ frontend/src/components/Teacher Components/Tile.jsx
@@ -1,19 +1,29 @@
 import DropDown from "./DropDown";
 import { useState } from "react";
+import { useNavigate } from "react-router";
 
 function Tile({ allotment }) {
+    const navigate = useNavigate();
     const [Open, setOpen] = useState(false);
     const toggle = () => setOpen(!Open);
+    const goToSubject = () => {
+        navigate(`/subjects/${allotment.subject.id}`);
+    };
     return (
         <div
             className={`m-auto p-4 rounded-l shadow-lg w-full aria-expanded=${Open}`}
-            onClick={toggle}
         >
-            <div className="button  aria-expanded={isOpen} cursor-pointer flex justify-between items-center w-full">
-                <div className="bg-cyan-500 shadow-lg shadow-cyan-500/50 p-4 rounded-l text-white">
+            <div className="button  aria-expanded={isOpen}  flex justify-between items-center w-full">
+                <div
+                    onClick={goToSubject}
+                    className="bg-cyan-500 shadow-lg cursor-pointer shadow-cyan-500/50 p-4 rounded-l text-white"
+                >
                     {allotment.subject.course_code}
                 </div>
-                <div className="text-[20px] pl-2 font-semibold">
+                <div
+                    onClick={toggle}
+                    className="text-[20px] pl-2 font-semibold cursor-pointer hover:text-blue-500"
+                >
                     {allotment.subject.name}
                 </div>
                 <div className="text-[22px] pl-2 font-semibold text-orange-700">
@@ -25,8 +35,8 @@ function Tile({ allotment }) {
             {Open ? (
                 <DropDown
                     lecture={allotment.allotted_lecture_hours}
-                    practical={allotment.allotted_tutorial_hours}
-                    tutorial={allotment.allotted_practical_hours}
+                    practical={allotment.allotted_practical_hours}
+                    tutorial={allotment.allotted_tutorial_hours}
                 />
             ) : null}
         </div>
