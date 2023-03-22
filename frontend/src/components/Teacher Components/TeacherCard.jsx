508f0fbdfdb9e973bae845747416d42b78277a31
chiggy69 <chiragaggarwal69@gmail.com>
2023-03-22 22:04:46 +0530

    current load display on teacher card

--- frontend/src/components/Teacher Components/TeacherCard.jsx
+++ frontend/src/components/Teacher Components/TeacherCard.jsx
@@ -1,7 +1,14 @@
 import { useNavigate } from "react-router-dom";
 import setColor from "../../helpers/setColor";
+import { Tag } from "@geist-ui/core";
 
-export function TeacherCard({ name, preferred_mode, id, assigned_status }) {
+export function TeacherCard({
+    name,
+    preferred_mode,
+    id,
+    assigned_status,
+    current_load,
+}) {
     const navigate = useNavigate();
     const handleClick = () => {
         navigate(`/teachers/${id}`);
@@ -19,11 +26,24 @@ export function TeacherCard({ name, preferred_mode, id, assigned_status }) {
                 className="h-40 w-72 rounded-xl object-cover"
             />
 
-            <div className="text-l font-sans font-bold ml-2">{name}</div>
-            <div className="flex gap-2 px-2 rounded-md bg-red-100 w-16 text-center m-2">
-                <div className="font-bold">{preferred_mode[0]}</div>
-                <div className="font-bold">{preferred_mode[1]}</div>
-                <div className="font-bold">{preferred_mode[2]}</div>
+            <div>
+                <div className="text-l font-sans font-bold ml-2 flex items-center justify-between">
+                    <div>{name}</div>
+                    {/* <div className="relative mr-[20px]">
+                        <Capacity value={current_load} limit={14} />
+                        <div className="absolute left-1/2 top-1/2 text-[8px] -translate-x-1/2 -translate-y-1/2">
+                            {current_load}
+                        </div>
+                    </div> */}
+                    <Tag invert type="default" mr="6px">
+                        Current Load {current_load}
+                    </Tag>
+                </div>
+                <div className="flex gap-2 px-2 rounded-md bg-red-100 w-16 text-center m-2">
+                    <div className="font-bold">{preferred_mode[0]}</div>
+                    <div className="font-bold">{preferred_mode[1]}</div>
+                    <div className="font-bold">{preferred_mode[2]}</div>
+                </div>
             </div>
         </div>
     );
