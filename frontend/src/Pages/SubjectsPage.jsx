22bf97a9535d1ca4faf6d01c2e474df57f6f6d5c
chiggy69 <chiragaggarwal69@gmail.com>
2023-03-28 22:14:56 +0530

    frontend-filter support

--- frontend/src/Pages/SubjectsPage.jsx
+++ frontend/src/Pages/SubjectsPage.jsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 import { request } from "../helpers/Client";
 import FetchingIndicator from "../components/common/FetchingIndicator";
 import { Pagination } from "../components/common/Pagination";
-import { Drawer } from "@geist-ui/core";
+import { Drawer, Popover, Tag, Checkbox } from "@geist-ui/core";
 import { FiSearch } from "react-icons/fi";
 import SubjectCard from "../components/Subject Components/SubjectCard";
 import { CustomSearch } from "../components/common/CustomSearch";
@@ -14,6 +14,9 @@ function SubjectsPage() {
     const navigate = useNavigate();
     const [page, setPage] = useState(1);
     const [search, setSearch] = useState(false);
+    const [coursetypeFilters, setCoursetypeFilters] = useState([]);
+    const [programmeFilters, setProgrammeFilters] = useState([]);
+    const [searchUsingFilters, setSearchUsingFilters] = useState(false);
     const subjectsQuery = useQuery(
         ["subjects", { page }],
         () =>
@@ -27,10 +30,49 @@ function SubjectsPage() {
         }
     );
 
+    const typeContent = [
+        { label: "Core", value: "core" },
+        { label: "Elective", value: "elec" },
+        { label: "Specialization", value: "spec" },
+    ];
+
+    const programmeContent = [
+        { label: "Btech", value: "btech" },
+        { label: "Mtech", value: "mtech" },
+        { label: "Phd", value: "phd" },
+    ];
+
+    const CheckBoxContent = (content, state, setState) => {
+        return (
+            <div className="p-4">
+                <Checkbox.Group
+                    value={state}
+                    onChange={(value) => setState(value)}
+                >
+                    <div className="flex flex-col gap-y-4 items-start">
+                        {content.map((item, idx) => (
+                            <Checkbox key={idx} value={item.value}>
+                                {item.label}
+                            </Checkbox>
+                        ))}
+                    </div>
+                </Checkbox.Group>
+            </div>
+        );
+    };
+
+    const handler = (value) => {
+        // console.log(value);
+        if (value == false) {
+            console.log(programmeFilters, coursetypeFilters);
+            setSearchUsingFilters(true);
+        }
+    };
+
     return (
         <div className="w-screen h-screen pt-2  overflow-x-hidden">
-            <div className="flex justify-between px-8 items-center">
-                <div className="flex gap-x-[100px] items-center ">
+            <div className="flex flex-col lg:flex-row  gap-y-4 justify-between px-8 items-center">
+                <div className="flex flex-col  md:flex-row gap-y-2.5 gap-x-[100px] items-center ">
                     <div className="title font-bold truncate relative flex items-center gap-x-4">
                         <div className=" text-[40px]">S U B J E C T S</div>
 
@@ -91,6 +133,36 @@ function SubjectsPage() {
                         Teachers
                     </div>
                 </div>
+            </div>
+
+            <div className="flex justify-center mt-6 gap-x-8">
+                <Popover
+                    // onVisibleChange={handler}
+                    content={() =>
+                        CheckBoxContent(
+                            typeContent,
+                            coursetypeFilters,
+                            setCoursetypeFilters
+                        )
+                    }
+                    className="cursor-pointer"
+                >
+                    <Tag>Course Type</Tag>
+                </Popover>
+
+                <Popover
+                    onVisibleChange={handler}
+                    content={() =>
+                        CheckBoxContent(
+                            programmeContent,
+                            programmeFilters,
+                            setProgrammeFilters
+                        )
+                    }
+                    className="cursor-pointer"
+                >
+                    <Tag>Programme</Tag>
+                </Popover>
             </div>
 
             {subjectsQuery.isError ? (
