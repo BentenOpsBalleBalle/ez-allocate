183818ec0c3a5b8939f35e2c09195ae4ac40d576
chiggy69 <chiragaggarwal69@gmail.com>
2023-03-28 22:12:40 +0530

    Not Found page (404)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import Subject from "./components/Subject";
 import LoginPage from "./Pages/LoginPage";
 import TeachersPage from "./Pages/TeachersPage";
 import Teacher from "./components/Teacher";
+import NotFoundError from "./Pages/NotFoundError";
 import { Routes, Route } from "react-router-dom";
 const App = () => {
     return (
@@ -14,6 +15,7 @@ const App = () => {
                 <Route path="/teachers" element={<TeachersPage />} />
                 <Route path="/teachers/:id" element={<Teacher />} />
                 <Route path="/login" element={<LoginPage />} />
+                <Route path="/*" element={<NotFoundError />} />
             </Routes>
         </div>
     );
