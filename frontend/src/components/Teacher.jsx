59f3635cc733247b93eb07241469a7cbc04b6ee5
chiggy69 <chiragaggarwal69@gmail.com>
2023-04-24 13:02:34 +0530

    preferences shown and navigation added

--- frontend/src/components/Teacher.jsx
+++ frontend/src/components/Teacher.jsx
@@ -4,22 +4,38 @@ import { request } from "../helpers/Client";
 import Tile from "./Teacher Components/Tile";
 import CircularApexChart from "./Teacher Components/ApexChart";
 import { useState, useMemo } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Teacher = () => {
     const { id } = useParams();
 
+    const navigate = useNavigate();
     const [totalLecture, setTotalLecture] = useState(0);
     const [totalTutorial, setTotalTutorial] = useState(0);
     const [totalPractical, setTotalPractical] = useState(0);
     const teacherAllotmentsQuery = useQuery(
         ["teachers", +id, "allotments"],
         () =>
-            client.send({
+            request.send({
                 url: `api/teachers/${id}/allotments`,
                 method: "GET",
                 service: "allocate",
             })
     );
+
+    const teachersChoiceQuery = useQuery(
+        ["teachers", +id, "choices"],
+        () =>
+            request.send({
+                url: `api/teachers/${id}/choices`,
+                method: "GET",
+                service: "allocate",
+            }),
+        {
+            staleTime: Infinity,
+        }
+    );
+
     useMemo(() => {
         // console.log("memo");
         teacherAllotmentsQuery.data?.data?.map((allotment) => {
@@ -67,13 +83,41 @@ const Teacher = () => {
                     })
                 )}
             </div>
-            <div className=" w-1/2 h-1/2 mr-10 rounded-md m-10 p-5 shadow-[0_9px_17px_-6px_rgba(0,0,0,0.2)]">
+            {teachersChoiceQuery.isLoading ? (
+                <p>Loading...</p>
+            ) : (
+                <div className="grid grid-cols-2 gap-x-10 gap-y-10 mr-10">
+                    {teachersChoiceQuery.data.data.map((choice) => {
+                        if (choice.choice_number != 0) {
+                            return (
+                                <div
+                                    className="bg-blue-100 cursor-pointer"
+                                    onClick={() => {
+                                        navigate(
+                                            `/subjects/${choice.subject.id}`
+                                        );
+                                    }}
+                                >
+                                    <div className="text-[20px] font-bold">
+                                        {choice.subject.name}-
+                                        {choice.subject.course_code}
+                                    </div>
+                                    <div className="text-[20px] ">
+                                        Choice Number: {choice.choice_number}
+                                    </div>
+                                </div>
+                            );
+                        }
+                    })}
+                </div>
+            )}
+            {/* <div className=" w-1/2 h-1/2 mr-10 rounded-md m-10 p-5 shadow-[0_9px_17px_-6px_rgba(0,0,0,0.2)]">
                 <CircularApexChart
                     lecture={totalLecture}
                     practical={totalPractical}
                     tutorial={totalTutorial}
                 />
-            </div>
+            </div> */}
         </div>
     );
 };
