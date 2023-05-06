449a6ded6c9558d4713d7bd25d999cfd0718363c
chiggy69 <chiragaggarwal69@gmail.com>
2023-05-07 02:15:14 +0530

    subjects card beautify

--- frontend/src/components/Teacher.jsx
+++ frontend/src/components/Teacher.jsx
@@ -2,17 +2,18 @@ import { useQuery } from "@tanstack/react-query";
 import { useParams } from "react-router-dom";
 import { request } from "../helpers/Client";
 import Tile from "./Teacher Components/Tile";
-import CircularApexChart from "./Teacher Components/ApexChart";
-import { useState, useMemo } from "react";
 import { useNavigate } from "react-router-dom";
+import { Badge, Button, useTheme } from "@geist-ui/core";
+import BreadCrumbComp from "./common/BreadCrumbComp";
 
 const Teacher = () => {
     const { id } = useParams();
+    const theme = useTheme();
 
     const navigate = useNavigate();
-    const [totalLecture, setTotalLecture] = useState(0);
-    const [totalTutorial, setTotalTutorial] = useState(0);
-    const [totalPractical, setTotalPractical] = useState(0);
+    // const [totalLecture, setTotalLecture] = useState(0);
+    // const [totalTutorial, setTotalTutorial] = useState(0);
+    // const [totalPractical, setTotalPractical] = useState(0);
     const teacherAllotmentsQuery = useQuery(
         ["teachers", +id, "allotments"],
         () =>
@@ -36,18 +37,18 @@ const Teacher = () => {
         }
     );
 
-    useMemo(() => {
-        // console.log("memo");
-        teacherAllotmentsQuery.data?.data?.map((allotment) => {
-            setTotalLecture((prev) => prev + allotment.allotted_lecture_hours);
-            setTotalTutorial(
-                (prev) => prev + allotment.allotted_tutorial_hours
-            );
-            setTotalPractical(
-                (prev) => prev + allotment.allotted_practical_hours
-            );
-        });
-    }, [teacherAllotmentsQuery.data?.data]);
+    // useMemo(() => {
+    //     // console.log("memo");
+    //     teacherAllotmentsQuery.data?.data?.map((allotment) => {
+    //         setTotalLecture((prev) => prev + allotment.allotted_lecture_hours);
+    //         setTotalTutorial(
+    //             (prev) => prev + allotment.allotted_tutorial_hours
+    //         );
+    //         setTotalPractical(
+    //             (prev) => prev + allotment.allotted_practical_hours
+    //         );
+    //     });
+    // }, [teacherAllotmentsQuery.data?.data]);
 
     const teacherQuery = useQuery(["teachers", +id], () =>
         request.send({
@@ -57,68 +58,76 @@ const Teacher = () => {
         })
     );
 
-    // if (teacherAllotmentsQuery.isLoading) return <div>Loaifng...</div>;
-
     return teacherQuery.isLoading ? (
         <div>loading...</div>
     ) : (
-        <div className="flex w-screen h-screen items-center justify-between  ">
-            <div className="card relative w-[55%] h-[86%] overflow-y-auto  rounded-md m-10 p-5 shadow-[0_9px_17px_-6px_rgba(0,0,0,0.2)]">
-                <div className="text-center  text-[30px] pt-10 font-bold">
-                    {teacherQuery.data?.data?.name}
-                </div>
-                <div className="text-[20px] pl-5 pb-5 font-semibold">
-                    Subjects Alloted
-                </div>
-                {teacherAllotmentsQuery.isLoading ? (
-                    <div>...</div>
-                ) : (
-                    teacherAllotmentsQuery.data?.data?.map((allotment) => {
-                        return (
-                            <Tile
-                                allotment={allotment}
-                                key={allotment.subject.id}
-                            />
-                        );
-                    })
-                )}
-            </div>
-            {teachersChoiceQuery.isLoading ? (
-                <p>Loading...</p>
-            ) : (
-                <div className="grid grid-cols-2 gap-x-10 gap-y-10 mr-10">
-                    {teachersChoiceQuery.data.data.map((choice) => {
-                        if (choice.choice_number != 0) {
+        <>
+            <BreadCrumbComp />
+            <div className="flex w-screen h-screen items-center justify-around  ">
+                <div className="card relative w-[55%] h-[86%] overflow-y-auto  rounded-md m-10 p-5 shadow-[0_9px_17px_-6px_rgba(0,0,0,0.2)]">
+                    <div className="text-center  text-[30px] pt-10 font-bold">
+                        {teacherQuery.data?.data?.name}
+                    </div>
+                    <div className="text-[20px] pl-5 pb-5 font-semibold">
+                        Subjects Alloted
+                    </div>
+                    {teacherAllotmentsQuery.isLoading ? (
+                        <div>...</div>
+                    ) : (
+                        teacherAllotmentsQuery.data?.data?.map((allotment) => {
                             return (
-                                <div
-                                    className="bg-blue-100 cursor-pointer"
-                                    onClick={() => {
-                                        navigate(
-                                            `/subjects/${choice.subject.id}`
-                                        );
-                                    }}
-                                >
-                                    <div className="text-[20px] font-bold">
-                                        {choice.subject.name}-
-                                        {choice.subject.course_code}
-                                    </div>
-                                    <div className="text-[20px] ">
-                                        Choice Number: {choice.choice_number}
-                                    </div>
-                                </div>
+                                <Tile
+                                    allotment={allotment}
+                                    key={allotment.subject.id}
+                                />
                             );
-                        }
-                    })}
+                        })
+                    )}
                 </div>
-            )}
-            {/* <div className=" w-1/2 h-1/2 mr-10 rounded-md m-10 p-5 shadow-[0_9px_17px_-6px_rgba(0,0,0,0.2)]">
+                {teachersChoiceQuery.isLoading ? (
+                    <p>Loading...</p>
+                ) : (
+                    <div className="grid grid-cols-2 gap-x-10 gap-y-10 mr-10">
+                        {teachersChoiceQuery.data.data.map((choice, idx) => {
+                            if (choice.choice_number != 0) {
+                                return (
+                                    <div key={idx}>
+                                        <Badge.Anchor>
+                                            <Badge
+                                                scale={0.6}
+                                                style={{
+                                                    backgroundColor:
+                                                        theme.palette.cyanDark,
+                                                }}
+                                            >
+                                                {choice.choice_number}
+                                            </Badge>
+                                            <Button
+                                                type="secondary"
+                                                onClick={() => {
+                                                    navigate(
+                                                        `/subjects/${choice.subject.id}`
+                                                    );
+                                                }}
+                                            >
+                                                {choice.subject.name}
+                                            </Button>
+                                        </Badge.Anchor>
+                                    </div>
+                                );
+                            }
+                        })}
+                    </div>
+                )}
+                {/* <div className=" w-1/2 h-1/2 mr-10 rounded-md m-10 p-5 shadow-[0_9px_17px_-6px_rgba(0,0,0,0.2)]">
                 <CircularApexChart
                     lecture={totalLecture}
                     practical={totalPractical}
                     tutorial={totalTutorial}
                 />
             </div> */}
-        </div>
+            </div>
+        </>
     );
 };
 
