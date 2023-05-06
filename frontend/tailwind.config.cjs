718ee421e9d6f79ac800c004f9bc2a901b91aa13
chiggy69 <chiragaggarwal69@gmail.com>
2023-05-07 02:11:19 +0530

    animation

--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -2,7 +2,17 @@
 module.exports = {
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
-        extend: {},
+        extend: {
+            keyframes: {
+                wiggle: {
+                    "0%, 100%": { transform: "rotate(-1.3deg)" },
+                    "50%": { transform: "rotate(1.3deg)" },
+                },
+            },
+            animation: {
+                wiggle: "wiggle 1s ease-in-out infinite",
+            },
+        },
     },
     plugins: [],
 };
