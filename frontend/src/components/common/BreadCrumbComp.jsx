29df49d7c602d85373cd698ec3ca611314b8bfed
chiggy69 <chiragaggarwal69@gmail.com>
2023-06-24 15:21:31 +0530

    Logout functionality

--- frontend/src/components/common/BreadCrumbComp.jsx
+++ frontend/src/components/common/BreadCrumbComp.jsx
@@ -1,4 +1,5 @@
 import { useNavigate } from "react-router-dom";
+import { FaSignOutAlt } from "react-icons/fa";
 
 const breadCrumbsMap = new Map();
 breadCrumbsMap.set("Home", "/subjects");
@@ -10,28 +11,39 @@ const BreadCrumbComp = () => {
     const handleClick = (e) => {
         navigate(breadCrumbsMap.get(e.target.innerText));
     };
+
+    const logout = () => {
+        localStorage.removeItem("token");
+        navigate("/");
+    };
     return (
-        <div className="flex text-gray-600 gap-x-1">
-            <div
-                onClick={handleClick}
-                className="cursor-pointer hover:text-blue-500"
-            >
-                Home
+        <div className="flex justify-between items-center">
+            <div className="flex text-gray-600 gap-x-1">
+                <div
+                    onClick={handleClick}
+                    className="cursor-pointer hover:text-blue-500"
+                >
+                    Home
+                </div>
+                /
+                <div
+                    className="cursor-pointer hover:text-blue-500"
+                    onClick={handleClick}
+                >
+                    Teachers
+                </div>
+                /
+                <div
+                    onClick={handleClick}
+                    className="cursor-pointer hover:text-blue-500"
+                >
+                    Files
+                </div>
             </div>
-            /
-            <div
-                className="cursor-pointer hover:text-blue-500"
-                onClick={handleClick}
-            >
-                Teachers
-            </div>
-            /
-            <div
-                onClick={handleClick}
-                className="cursor-pointer hover:text-blue-500"
-            >
-                Files
-            </div>
+            <FaSignOutAlt
+                onClick={logout}
+                className="cursor-pointer hover:text-red-500 mr-10 text-xl mt-1"
+            />
         </div>
     );
 };
